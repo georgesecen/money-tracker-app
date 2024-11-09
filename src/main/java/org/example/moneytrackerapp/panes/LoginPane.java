253a6062617b06e0ab506dcc2d827d0aac0c8de8
George <W0533223@myscc.ca>
2024-11-09 15:29:16 -0500

    Create credentials file
    - Create credentials file with an empty object if it does not exist
    
    - Added credentials.json file to .gitignore

--- src/main/java/org/example/moneytrackerapp/panes/LoginPane.java
+++ src/main/java/org/example/moneytrackerapp/panes/LoginPane.java
@@ -2,10 +2,29 @@ package org.example.moneytrackerapp.panes;
 
 import javafx.scene.layout.BorderPane;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Contains sign in/sign up and load/save user credentials functionality.
  */
 public class LoginPane extends BorderPane {
 
-    public LoginPane(){}
+    public LoginPane(){
+
+        File credentialsFile = new File("src/main/java/org/example/moneytrackerapp/credentials.json");
+
+        // If credentials file does not exist create it with an empty object
+        if (!credentialsFile.exists()){
+            try {
+                FileWriter writer = new FileWriter(credentialsFile.getPath());
+                writer.write("{}");
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Error creating file in LoginPane.java");
+                e.printStackTrace();
+            }
+        }
+    }
 }
