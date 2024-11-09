f0fa766b0121551b9958da19e7b2a5b15c93447b
George <W0533223@myscc.ca>
2024-11-09 15:16:12 -0500

    Add LoginPane and LoginScene
    - Added LoginPane which will take care of user sign in/sign up. LoginPane will also take
    care of saving and loading user credentials from file. This way the user does not have to
    login again with password after previously logging in.
    
    - Added LoginScene to house the LoginPane and screen dimensions for the scene

---
+++ src/main/java/org/example/moneytrackerapp/panes/LoginPane.java
@@ -0,0 +1,11 @@
+package org.example.moneytrackerapp.panes;
+
+import javafx.scene.layout.BorderPane;
+
+/**
+ * Contains sign in/sign up and load/save user credentials functionality.
+ */
+public class LoginPane extends BorderPane {
+
+    public LoginPane(){}
+}
