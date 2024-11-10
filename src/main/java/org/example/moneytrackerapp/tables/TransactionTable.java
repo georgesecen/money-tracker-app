419706823480a4d63466c3107d3488316607a89b
Wania Sharif <w0842810@myscc.ca>
2024-11-09 22:49:19 -0500

    Create getAllTransactions method

--- src/main/java/org/example/moneytrackerapp/tables/TransactionTable.java
+++ src/main/java/org/example/moneytrackerapp/tables/TransactionTable.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import static org.example.moneytrackerapp.database.DBConst.TABLE_TRANSACTIONS;
+import static org.example.moneytrackerapp.database.DBConst.*;
 
 /**
  * TransactionTable class represents a single table of Transactions.
@@ -18,5 +18,29 @@ public class TransactionTable {
     Database db = Database.getInstance();
     ArrayList<Transaction> transactions;
 
-    
+    /**
+     * This method creates and returns an ArrayList of the records in Transactions Table
+     * @return ArrayList consisting of the transactions in Transactions table
+     */
+    public ArrayList<Transaction> getAllTransactions() {
+        String query = "SELECT * FROM " + TABLE_TRANSACTIONS;
+
+        transactions = new ArrayList<>();
+        try {
+            Statement getTransactions = db.getConnection().createStatement();
+            ResultSet resultSet = getTransactions.executeQuery(query);
+
+            while (resultSet.next()) {
+                resultSet.getInt(TRANS_COLUMN_ID);
+                resultSet.getDouble(TRANS_COLUMN_AMOUNT);
+                resultSet.getString(TRANS_COLUMN_DESC);
+                resultSet.getString(TRANS_COLUMN_DATE);
+                resultSet.getInt(TRANS_COLUMN_CAT);
+            }
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return transactions;
+    }
 }
