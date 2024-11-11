013d405a3406f1d6dc1fb1a99e19dd68ead107fa
George <W0533223@myscc.ca>
2024-11-11 15:25:20 -0500

    Show LoginScene on application run

--- src/main/java/org/example/moneytrackerapp/HelloApplication.java
+++ src/main/java/org/example/moneytrackerapp/HelloApplication.java
@@ -1,18 +1,17 @@
 package org.example.moneytrackerapp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.moneytrackerapp.scenes.LoginScene;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        LoginScene scene = new LoginScene();
+        stage.setTitle("Works");
         stage.setScene(scene);
         stage.show();
     }
