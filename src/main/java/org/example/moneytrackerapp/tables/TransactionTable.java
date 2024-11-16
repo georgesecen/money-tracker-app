c9c047b4d28b5501566d6291af2ceb2ef501469c
Wania Sharif <w0842810@myscc.ca>
2024-11-16 11:22:36 -0500

    Add getInstance method

--- src/main/java/org/example/moneytrackerapp/tables/TransactionTable.java
+++ src/main/java/org/example/moneytrackerapp/tables/TransactionTable.java
@@ -12,8 +12,13 @@ import java.util.ArrayList;
 import static org.example.moneytrackerapp.database.DBConst.*;
 
 public class TransactionTable implements TransactionDAO {
+    private static TransactionTable instance;
+    private TransactionTable(){
+        db = Database.getInstance();
+    }
     Database db = Database.getInstance();
     ArrayList<Transaction> transactions;
+
     @Override
     public ArrayList<Transaction> getAllTransactions() {
         String query = "SELECT * FROM " + TABLE_TRANSACTIONS;
@@ -90,5 +95,12 @@ public class TransactionTable implements TransactionDAO {
                 + transaction.getId() + ", " + transaction.getAmt()
                 + ", " + transaction.getDesc() + ", " + transaction.getDate()
                 + ", " + transaction.getCat_id() + ")";
+    }
+
+    public static TransactionTable getInstance(){
+        if(instance == null){
+            instance = new TransactionTable();
+        }
+        return instance;
     }
 }
