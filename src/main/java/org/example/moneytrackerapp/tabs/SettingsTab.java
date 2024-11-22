84ab7f765ccb5805752bbb0fd843b72d9e156a89
Wania Sharif <w0842810@myscc.ca>
2024-11-21 19:30:57 -0500

    Add some page elements * Create a root BorderPane and set it as content * Add title, set it as top, center it * Create headers * Create a vbox for page body, add headers to it and set as center

--- src/main/java/org/example/moneytrackerapp/tabs/SettingsTab.java
+++ src/main/java/org/example/moneytrackerapp/tabs/SettingsTab.java
@@ -1,6 +1,12 @@
 package org.example.moneytrackerapp.tabs;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import org.example.moneytrackerapp.tables.CategoryTable;
 
 /**
  * SettingsTab class represents the Settings page. This will display to the user
@@ -16,6 +22,28 @@ public class SettingsTab extends Tab {
     public SettingsTab() {
         this.setText("Settings");
 
+        BorderPane root = new BorderPane();
+
+        CategoryTable categoryTable = CategoryTable.getInstance();
+
+        Text title = new Text("Settings");
+
+        Text addCategoryLabel = new Text("Add Category");
+
+        Text appearance = new Text("Appearance");
+
+        VBox body = new VBox(addCategoryLabel, appearance);
+        body.setSpacing(30);
+
+        root.setTop(title);
+        root.setAlignment(title, Pos.BOTTOM_CENTER);
+
+        root.setCenter(body);
+
+        root.setMargin(title, new Insets(100, 30, 30, 30));
+        root.setMargin(body, new Insets(30, 200, 30, 200));
+
+        this.setContent(root);
     }
 
     public static SettingsTab getInstance(){
