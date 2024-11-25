5dc219bac5f5b6ed843ff915037fdc0138348306
Wania Sharif <w0842810@myscc.ca>
2024-11-24 23:25:26 -0500

    #60 Add input fields

--- src/main/java/org/example/moneytrackerapp/tabs/AddCategoryPane.java
+++ src/main/java/org/example/moneytrackerapp/tabs/AddCategoryPane.java
@@ -1,6 +1,10 @@
 package org.example.moneytrackerapp.tabs;
 
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import org.example.moneytrackerapp.tables.CategoryTable;
 import org.example.moneytrackerapp.tables.TransactionTable;
 
@@ -13,6 +17,27 @@ public class AddCategoryPane extends GridPane {
     public AddCategoryPane() {
         CategoryTable categoryTable = CategoryTable.getInstance();
         TransactionTable transactionTable = TransactionTable.getInstance();
+
+        Text nameLabel = new Text("Category Name");
+        TextField name = new TextField();
+
+        Text typeLabel = new Text("Category type");
+        ToggleGroup typeToggleGroup = new ToggleGroup();
+        RadioButton type1 = new RadioButton("Income");
+        type1.setToggleGroup(typeToggleGroup);
+        RadioButton type2 = new RadioButton("Expense");
+        type2.setToggleGroup(typeToggleGroup);
+        type2.setSelected(true);
+
+        this.add(nameLabel, 0, 0);
+        this.add(name, 1, 0);
+
+        this.add(typeLabel, 0, 1);
+        this.add(type1, 1, 1);
+        this.add(type2, 2, 1);
+
+
+
 
     }
 }
