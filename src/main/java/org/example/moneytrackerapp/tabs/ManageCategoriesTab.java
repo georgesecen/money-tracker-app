928919bd4e3a43e3e01a2d985ad6edda71a50abe
Wania Sharif <w0842810@myscc.ca>
2024-11-25 21:28:16 -0500

    Refine layout * Set margins * Align items to center

--- src/main/java/org/example/moneytrackerapp/tabs/ManageCategoriesTab.java
+++ src/main/java/org/example/moneytrackerapp/tabs/ManageCategoriesTab.java
@@ -1,10 +1,13 @@
 package org.example.moneytrackerapp.tabs;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import org.example.moneytrackerapp.pojo.Category;
 import org.example.moneytrackerapp.tables.CategoryTable;
 
@@ -25,20 +28,28 @@ public class ManageCategoriesTab extends Tab {
         Text title = new Text("Manage Categories");
 
         Text userCategories = new Text("Your categories");
+        userCategories.setTextAlignment(TextAlignment.CENTER);
         ComboBox<Category> categories = new ComboBox<>();
 
         GridPane content = new GridPane();
         content.setHgap(10);
         content.setVgap(10);
         content.add(userCategories, 0, 0,3,1);
-        content.add(categories, 1, 0);
+        content.add(categories, 4, 0);
 
 
         // Set up the view
         BorderPane root = new BorderPane();
 
         root.setTop(title);
+        root.setAlignment(title, Pos.CENTER);
+        root.setMargin(title, new Insets(100, 30, 30, 30));
+
+
         root.setCenter(content);
+        root.setAlignment(content, Pos.CENTER);
+        root.setMargin(content, new Insets(30, 200, 30, 200));
+
         this.setContent(root);
     }
 
