d8cd0b5d05a6c100ca56f33a9b00a1c974f83400
Wania Sharif <w0842810@myscc.ca>
2024-11-25 20:50:29 -0500

    Create gridpane to hold content, set it as center

--- src/main/java/org/example/moneytrackerapp/tabs/ManageCategoriesTab.java
+++ src/main/java/org/example/moneytrackerapp/tabs/ManageCategoriesTab.java
@@ -1,13 +1,16 @@
 package org.example.moneytrackerapp.tabs;
 
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import org.example.moneytrackerapp.pojo.Category;
 import org.example.moneytrackerapp.tables.CategoryTable;
 
 /**
  * ManageCategoriesTab class represents a page that will allow the user
- * to customize the app by changing styles or adding additional
+ * to customize the app by deleting or adding additional
  * category types.
  * @author Wania Sharif
  */
@@ -21,10 +24,21 @@ public class ManageCategoriesTab extends Tab {
 
         Text title = new Text("Manage Categories");
 
+        Text userCategories = new Text("Your categories");
+        ComboBox<Category> categories = new ComboBox<>();
+
+        GridPane content = new GridPane();
+        content.setHgap(10);
+        content.setVgap(10);
+        content.add(userCategories, 0, 0,3,1);
+        content.add(categories, 1, 0);
+
+
         // Set up the view
         BorderPane root = new BorderPane();
 
         root.setTop(title);
+        root.setCenter(content);
         this.setContent(root);
     }
 
