6e6a7c17eef7ee648e0567d8c0b58ba16246d7cf
Wania Sharif <w0842810@myscc.ca>
2024-11-25 20:26:32 -0500

    #62 Setup title and root pane

--- src/main/java/org/example/moneytrackerapp/tabs/ManageCategoriesTab.java
+++ src/main/java/org/example/moneytrackerapp/tabs/ManageCategoriesTab.java
@@ -1,6 +1,9 @@
 package org.example.moneytrackerapp.tabs;
 
 import javafx.scene.control.Tab;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import org.example.moneytrackerapp.tables.CategoryTable;
 
 /**
  * ManageCategoriesTab class represents a page that will allow the user
@@ -11,7 +14,19 @@ import javafx.scene.control.Tab;
 public class ManageCategoriesTab extends Tab {
     private static ManageCategoriesTab instance;
 
-    public ManageCategoriesTab() {}
+    public ManageCategoriesTab() {
+        this.setText("Manage Categories");
+
+        CategoryTable categoryTable = CategoryTable.getInstance();
+
+        Text title = new Text("Manage Categories");
+
+        // Set up the view
+        BorderPane root = new BorderPane();
+
+        root.setTop(title);
+        this.setContent(root);
+    }
 
     public static ManageCategoriesTab getInstance(){
         if(instance == null){
